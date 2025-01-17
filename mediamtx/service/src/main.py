4ba932e846a87e2a4a202875e357060166966371
xrainie <xoxrainie@gmail.com>
2025-01-17 16:46:54 +0300

    fix

--- mediamtx/service/src/main.py
+++ mediamtx/service/src/main.py
@@ -14,36 +14,36 @@ logger = logging.getLogger()
 
 def init(pg_dsn: str):
     logger.info("Service starting")
-    loop = asyncio.get_event_loop()    
-    state = loop.run_until_complete(init_state(pg_dsn, loop))
-    if state is None:
-        sys.exit(2)
-    else:
-        logger.info("Service started")
+    # loop = asyncio.get_event_loop()
+    # state = loop.run_until_complete(init_state(pg_dsn, loop))
+    # if state is None:
+    #     sys.exit(2)
+    # else:
+    #     logger.info("Service started")
 
-        try:
-            loop.run_forever()
-        except (SystemExit, KeyboardInterrupt):
-            logger.info("Service stopping")
-        except e:
-            logger.exception(f"Service crashed {e}")
-        finally:
-            loop.run_until_complete(shutdown(state))
-            logger.info("Service stopped")
-
-
-async def init_state(pg_dsn: str, loop: AbstractEventLoop):
-    state = None
-    db = DBI(pg_dsn)
-
-    if await db.connect():
-        state = State(loop, db)
-    else: 
-        logger.critical(f"Can't make initial connect to PostgreSQL {pg_dsn}")
-
-    return state
+    #     try:
+    #         loop.run_forever()
+    #     except (SystemExit, KeyboardInterrupt):
+    #         logger.info("Service stopping")
+    #     except e:
+    #         logger.exception(f"Service crashed {e}")
+    #     finally:
+    #         loop.run_until_complete(shutdown(state))
+    #         logger.info("Service stopped")
 
 
-async def shutdown(state):
-    if state and state.db:
-        await state.db.done()
+# async def init_state(pg_dsn: str, loop: AbstractEventLoop):
+#     state = None
+#     db = DBI(pg_dsn)
+
+#     if await db.connect():
+#         state = State(loop, db)
+#     else:
+#         logger.critical(f"Can't make initial connect to PostgreSQL {pg_dsn}")
+
+#     return state
+
+
+# async def shutdown(state):
+#     if state and state.db:
+#         await state.db.done()
